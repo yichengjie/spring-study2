ff9a22ead8294ae1a0b2e0827b689ca5c9468223
易成杰 <626659321@qq.com>
2020-05-24 16:31:29 +0800

    保存

---
+++ simple-framework/src/main/java/org/simpleframework/core/BeanContainer.java
@@ -0,0 +1,13 @@
+package org.simpleframework.core;
+
+/**
+ * ClassName: BeanContainer
+ * Description: TODO(描述)
+ * Date: 2020/5/24 16:06
+ *
+ * @author yicj(626659321 @ qq.com)
+ * 修改记录
+ * @version 产品版本信息 yyyy-mm-dd 姓名(邮箱) 修改信息
+ */
+public class BeanContainer {
+}
